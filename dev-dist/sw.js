d513a0e6ca73ff53356d31921923398b1ead32b5
abrown84 <konfliktquake@gmail.com>
2025-08-21 20:49:05 -0700

    Force redeploy

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ga04m59sllg"
+    "revision": "0.l2h5968108o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
